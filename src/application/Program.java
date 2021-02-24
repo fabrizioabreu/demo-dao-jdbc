d860ba19f94a2a75c808d6322e4c50ffac517802
Fabrizio Abreu <fabrizioabreu@live.com>
2021-02-24 18:27:04 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,12 +10,10 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		
-		Department obj = new Department(1, "Books");
-		
-		Seller seller = new Seller(12, "Fabrizio", "fabrizio@gmail.com", new Date(), 3000.00, obj);
-		
+				
 		SellerDao sellerDao = DaoFactory.cerateSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 
