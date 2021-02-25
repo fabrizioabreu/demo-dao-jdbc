5d72635f4e2103ec88e72ca6e595ab4b537fad77
Fabrizio Abreu <fabrizioabreu@live.com>
2021-02-25 11:41:29 -0300

    Reusing instantiation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -5,7 +5,7 @@ import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	
-	public static SellerDao cerateSellerDao() {
+	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
 	}
 
