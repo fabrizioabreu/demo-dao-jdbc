e3f6694f06a54fe7423d3136c5ba8113a50a2944
Fabrizio Abreu <fabrizioabreu@live.com>
2021-02-26 12:52:54 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,7 +12,9 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-				
+		
+		Scanner sc = new Scanner(System.in);
+		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(); // Aula 248
@@ -48,6 +51,14 @@ public class Program {
 		sellerDao.update(seller);
 		System.out.println("Update Competed!");
 		
+		System.out.println(); // Aula 253
+		System.out.println("====== TEST 6: seller DELETE ======");
+		System.out.println("Digite o ID para ser deletado: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete Completed!");
+		
+		sc.close();
 	}
 
 }
