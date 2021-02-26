87f9dee7968139207278b9e09c4b73d68a080d72
Fabrizio Abreu <fabrizioabreu@live.com>
2021-02-26 12:16:49 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,6 +40,14 @@ public class Program {
 		Seller newSeller = new Seller(null, "Natasha", "natasha@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Insert! New Id = " + newSeller.getId());
+		
+		System.out.println(); // Aula 252
+		System.out.println("====== TEST 5: seller UPDATE ======");
+		seller = sellerDao.findById(1);
+		seller.setName("João do Caminhão");
+		sellerDao.update(seller);
+		System.out.println("Update Competed!");
+		
 	}
 
 }
